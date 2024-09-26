2a11e5471ad71fe7f33e9aecc4fea3821e1bcc9c
Arend-Jan van Hilten <arendjan18@gmail.com>
2024-09-26 10:52:18 +0200

    dont rm armbian list anymore

--- mirte_main_install.sh
+++ mirte_main_install.sh
@@ -14,7 +14,7 @@ echo "nameserver 8.8.8.8" >/etc/resolv.conf || true
 # fi
 nslookup ports.ubuntu.com || true
 
-sudo rm /etc/apt/sources.list.d/armbian.list || true
+# sudo rm /etc/apt/sources.list.d/armbian.list || true
 
 chown root:root /usr/bin/sudo && chmod 4755 /usr/bin/sudo # something with sudo otherwise complaining about "sudo: /usr/bin/sudo must be owned by uid 0 and have the setuid bit set"
 . /usr/local/src/mirte/settings.sh                        # load settings
