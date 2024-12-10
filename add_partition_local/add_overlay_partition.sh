11bd4d2f1dc7df757480302540c447e3548cfa46
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2024-12-10 09:20:42 +0100

    Add overlay partition

---
+++ add_partition_local/add_overlay_partition.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+set -ex
+
+image_file=$1
+parent_path=$(
+	cd "$(dirname "${BASH_SOURCE[0]}")"
+	pwd -P
+)
+
+add_partition() {
+	startLocation=$(sfdisk -l -o start -N1 "$image_file" | tail -1)
+	# should be 40960 for zero2, 8192 for zero1
+
+	extraSize="1G"
+
+	dd if=/dev/zero bs=1M count=1024 >>"$image_file"
+	echo "+$extraSize" | sfdisk --move-data -N 1 "$image_file"
+	echo "$startLocation, $extraSize, b" | sfdisk -a "$image_file"
+	sleep 5
+	loop=$(kpartx -av "$image_file")
+	echo $loop
+	loopvar=$(echo $loop | grep -oP 'loop[0-9]*' | head -1)
+	echo $loopvar
+	mkfs.ext4 /dev/mapper/${loopvar}p2 -n "mirte_root"
+	sleep 5
+	kpartx -dv /dev/${loopvar}
+}
+
+if sfdisk -l "$image_file" | grep -q '.img2'; then
+	echo "Already contains extra partition, only copying default files"
+else
+	add_partition
+fi
+
+
+$parent_path/../pishrink.sh "$image_file"
+
+echo "done"
--- scripts/finalize.sh
+++ scripts/finalize.sh
@@ -4,6 +4,7 @@ imagefile=$1
 if $INSTALL_PROVISIONING; then
 	sudo ./add_partition_local/add_partition.sh $imagefile
 fi
+sudo ./add_partition_local/add_overlay_partition.sh $imagefile
 sudo ./pishrink.sh $imagefile || true
 filename=$(basename $imagefile .img)
 newImageFile="build/${filename}_$(date +"%Y-%m-%d_%H_%M_%S").img"
